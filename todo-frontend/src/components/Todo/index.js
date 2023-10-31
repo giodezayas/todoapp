b2122ac8ee589cd26e752d17f4a7a6a8fb995933
GiovannideZayasAngel <giovanni.zayas@smabit.cu>
2023-10-31 10:59:16 -0400

    inicial setup for Frontend

---
+++ todo-frontend/src/components/Todo/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Container } from './style'
+
+function Todo (){
+    return (
+        <Container>
+            <h2>List of Todos</h2>
+        </Container>
+    )
+}
+
+export default Todo
