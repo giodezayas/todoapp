14bdb4dc04de321053667eb5c24b1a6b4c604c86
GiovannideZayasAngel <giovanni.zayas@smabit.cu>
2023-10-31 16:02:56 -0400

    Props for Form funcionality

--- todo-frontend/src/components/Form/index.js
+++ todo-frontend/src/components/Form/index.js
@@ -1,33 +1,44 @@
-import React from 'react'
-import { Button, FormContainer,Innut,Innut2,Select } from './style'
+import React from 'react';
+import { Button, FormContainer, Innut,Innut2, Select } from './style';
 
-function Form (){
+function Form(props) {
+    const {
+        inputValue1,
+        setInputValue1,
+        inputValue2,
+        setInputValue2,
+        selectedValue,
+        setSelectedValue
+    } = props;
+
+    
+
     return (
-        <FormContainer>
+        <FormContainer >
             <Innut
-            value=''
-            type='text'
-            role='input'
+                value={inputValue1}
+                type='text'
+                onChange={(e)=> setInputValue1(e.target.value)}
+                role='input'
             />
             <Innut2
-            value=''
-            type='text'
-            role='input'/>
-            <Select
-            value=''
-            role='dropdown'
-            >
-            <option value="" disabled hidden>
-                Escoge la categoría
-            </option>
-            <option value="WORK">Work</option>
-            <option value="HOME"> Home</option>
-            <option value="LEARNING">Learning</option>
+                value={inputValue2}
+                type='text'
+                onChange={(e)=> setInputValue2(e.target.value)}
+                role='input'
+            />
+            <Select value={selectedValue} onChange={(e)=> setSelectedValue(e.target.value)} role='dropdown'>
+                <option value="" disabled hidden>
+                    Escoge la categoría
+                </option>
+                <option value="WORK">Work</option>
+                <option value="HOME">Home</option>
+                <option value="LEARNING">Learning</option>
             </Select>
-            
+            <br />
             <Button type='submit'>Add</Button>
         </FormContainer>
-    )
+    );
 }
 
-export default Form 
+export default Form;
--- todo-frontend/src/components/Todo/index.js
+++ todo-frontend/src/components/Todo/index.js
@@ -1,12 +1,25 @@
-import React from 'react'
+import React,{useState} from 'react'
 import { Container } from './style'
 import Form from '../Form'
 
 function Todo (){
+    const [inputValue1, setInputValue1] = useState('');
+    const [inputValue2, setInputValue2] = useState('');
+    const [selectedValue, setSelectedValue] = useState('');
+    console.log(inputValue1,"titulo")
+    console.log(inputValue2,"descripcion")
+    console.log(selectedValue,"categoria")
     return (
         <Container>
             <h2 style={{ textAlign: 'center' }}>Lista de Tareas</h2>
-            <Form/>
+            <Form
+                inputValue1={inputValue1}
+                setInputValue1={setInputValue1}
+                inputValue2={inputValue2}
+                setInputValue2={setInputValue2}
+                selectedValue={selectedValue}
+                setSelectedValue={setSelectedValue}
+            />
         </Container>
     )
 }
